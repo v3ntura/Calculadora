0652aef903d65eb5d72b3215a47b3ccd258ea5f0
yventura <52053283+yventura@users.noreply.github.com>
2020-07-19 08:52:33 -0300

    Finalizado
    Sistema funcional, falta realizar o geramento do APK

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
+
+
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
+n1:number;
+n2:number;
+nR:number;
+
   constructor() {}
+
+  Calculadora()
+  {
+    this.nR = this.n1 * this.n2;
+  }
 
 }
